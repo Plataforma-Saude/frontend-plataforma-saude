095b83c53a6d145f7f25fafb9e76a3e8a4d2b551
Mauricio Melo <mauriciotmelo@yahoo.com.br>
2025-08-21 00:19:32 -0300

    Refactor EmailInput and PasswordInput components for improved accessibility and error handling; update LoginScreen layout and styling

--- frontend/src/components/EmailInput/EmailInput.jsx
+++ frontend/src/components/EmailInput/EmailInput.jsx
@@ -1,8 +1,38 @@
-export default function EmailInput({label, containerStyle, labelStyle, inputStyle, placeholder}) {
-    return(
-        <div className={containerStyle}>
-            <label className={labelStyle} htmlFor="email">{label}</label>
-            <input className={inputStyle} type="email" id="email" placeholder={placeholder} />
+export default function EmailInput({
+    label,
+    name,
+    placeholder,
+    register,
+    disabled,
+    errors,
+    containerClassName,
+    labelClassName,
+    inputClassName,
+    errorClassName,
+}) {
+    const hasError = !!errors;
+
+    const errorClasses = 'border-2 border-red-400';
+
+    return (
+        <div className={containerClassName}>
+            <label className={labelClassName} htmlFor={name}>
+                {label}
+            </label>
+
+            <input
+                className={`${inputClassName} ${hasError ? errorClasses : ''}`}
+                type="email"
+                id={name}
+                name={name}
+                placeholder={placeholder}
+                disabled={disabled}
+                {...register}
+            />
+
+            {hasError && (
+                <p className={errorClassName}>{errors.message}</p>
+            )}
         </div>
-    )
+    );
 }
