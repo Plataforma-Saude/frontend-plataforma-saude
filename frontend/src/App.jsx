d11a57f6158b8beac7c2ee0df34576238318b2ae
Mauricio Melo <mauriciotmelo@yahoo.com.br>
2025-09-10 22:40:15 -0300

    Feat: add routing with react-router-dom and implement Header component; create SelectDoctor page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import LoginScreen from "./pages/LoginScreen";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import SelectDoctor from "./pages/SelectDoctor";
 
 export default function App() {
-    return(
-        
-        <LoginScreen />
-        
-    )
+    return (
+        <Router>
+            <Routes>
+                {/* Redireciona para /login caso a rota não seja reconhecida */}
+                <Route path="*" element={<Navigate to="/login" replace />} />
+                <Route path="/login" element={<LoginScreen />} />
+                <Route path="/buscar-medico" element={<SelectDoctor />} />
+            </Routes>
+        </Router>
+    );
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return(
+        <header>
+            <div>
+                <figure>
+                    <img src="" alt="Logo da clínica" />
+                </figure>
+                <h2>NOME DA CLÍNICA</h2>
+            </div>
+            
+            <div>
+                <p>Bem vindo, <span>USER</span></p>
+                <figure>
+                    <img src="" alt="Foto do usuário" />
+                </figure>
+            </div>
+        </header>
+    )
+}
