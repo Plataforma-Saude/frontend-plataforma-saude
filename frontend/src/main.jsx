82296bf6eb811dcbc62f229d0bfa85e3f02468e6
Natália Peralta <nati.g.peralta@hotmail.com>
2025-09-29 23:00:59 -0300

    fix/f-60

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import LoginScreen from './pages/LoginScreen';
 import RegisterScreen from './pages/RegisterScreen';
 import MyAppointmentsPage from './pages/patient/MyAppointmentsPage';
 import './index.css';
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
         <Route path="/minhas-consultas" element={<MyAppointmentsPage />} />
-        <App />
       </Routes>
     </BrowserRouter>
   </StrictMode>
